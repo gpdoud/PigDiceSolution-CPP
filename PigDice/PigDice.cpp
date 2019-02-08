93643b9341870531173af13a840eaffff663b1d7
Greg Doud <gpdoud@gmail.com>
2019-02-08 16:32:31 -0500

    Set WR to 700

--- PigDice/PigDice.cpp
+++ PigDice/PigDice.cpp
@@ -1,9 +1,17 @@
 #include "pch.h"
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <iomanip>
+
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
 int roll_die() {
-	return rand() % 6 + 1;
+	//return rand() % 6 + 1;
+	return rng() % 6 + 1;
 }
 int play() {
 	int score = 0;
@@ -14,17 +22,18 @@ int play() {
 	return score;
 }
 int main() {
-	int world_record = 606;
+//	srand(time(NULL));
+	int world_record = 700;
 	int high_score = 0;
-	int games = 0;
+	unsigned long long games = 0;
 	while(high_score <= world_record) {
 		int score = play();
 		games++;
 		if(score > high_score) {
 			high_score = score;
 			std::cout << "WR is " << world_record 
-				<< ", new HS is " << high_score
-				<< " on game " << games << std::endl;
+				<< ", new HS is " << std::setw(3) << std::right << high_score
+				<< " on game " << std::setw(16) << std::right << games << std::endl;
 		}
 	}
 	std::cout << "New WR is " << high_score << std::endl;
